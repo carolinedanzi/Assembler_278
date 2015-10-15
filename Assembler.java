8e5edb7b874a1612dc9781038f32d9b699d8d573
Caroline Danzi <danzicr@miamioh.edu>
2015-10-15 18:35:06 -0400

    Implemented part of I format

--- Assembler.java
+++ Assembler.java
@@ -29,7 +29,6 @@ public class Assembler {
 			
 			// Prepare to print output to an output file
 			PrintWriter pw = new PrintWriter("output.txt");	
-			pw.println(line);
 			
 			String line;
 			String instruction;
@@ -66,105 +65,6 @@ public class Assembler {
 			System.out.println(e);
 		}		
 	}	
-	
-	/**
-	* This sets up the HashTables for the instruction types and 
-	* registers
-	*/
-	public static void setup() {
-		// TODO: We are missing a few instructions, such as bne and jr
-		
-		// Instruction type 1 codes are Register codes
-		// Instruction type 2 codes are Immediate codes
-		// lw, sw, addi, ori, andi, slti, beq, e.g. are Immediate codes
-		// add, sub, or, and, slt, sll, srl, jr e.g. are Register codes
-		// j and jal are Jump Commands
-		instTypeTable = new HashMap<String, Integer>();
-		instTypeTable.put("add", 1);  	//Register codes
-		instTypeTable.put("sub", 1);
-		instTypeTable.put("or", 1);
-		instTypeTable.put("and", 1);
-		instTypeTable.put("slt", 1);
-		instTypeTable.put("sll", 1);
-		instTypeTable.put("srl", 1);
-		instTypeTable.put("jr", 1);
-		instTypeTable.put("addi", 2);  	//Immediate codes
-		instTypeTable.put("ori", 2);
-		instTypeTable.put("lw", 2);
-		instTypeTable.put("sw", 2);
-		instTypeTable.put("andi", 2);
-		instTypeTable.put("slti", 2);
-		instTypeTable.put("beq", 2);
-		instTypeTable.put("bne", 2);
-		instTypeTable.put("move", 2);
-		instTypeTable.put("j", 3);  		//Jump Commands
-		instTypeTable.put("jal", 3); 
-		instTypeTable.put("jr", 3);
-		
-		// The register table
-		// The registers are translated from their common name, such as
-		// t0, to their 5-bit binary representation.
-		registerTable = new HashMap<String, Integer>();
-		registerTable.put("$zero", 0b00000);
-		registerTable.put("$at", 0b00001);
-		registerTable.put("$v0", 0b00010);
-		registerTable.put("$v1", 0b00011);
-		registerTable.put("$a0", 0b00100);
-		registerTable.put("$a1", 0b00101);
-		registerTable.put("$a2", 0b00110);
-		registerTable.put("$a3", 0b00111);
-		registerTable.put("$t0", 0b01000);
-		registerTable.put("$t1", 0b01001);
-		registerTable.put("$t2", 0b01010);
-		registerTable.put("$t3", 0b01011);
-		registerTable.put("$t4", 0b01100);
-		registerTable.put("$t5", 0b01101);
-		registerTable.put("$t6", 0b01110);
-		registerTable.put("$t7", 0b01111);
-		registerTable.put("$s0", 0b10000);
-		registerTable.put("$s1", 0b10001);
-		registerTable.put("$s2", 0b10010);
-		registerTable.put("$s3", 0b10011);
-		registerTable.put("$s4", 0b10100);
-		registerTable.put("$s5", 0b10101);
-		registerTable.put("$s6", 0b10110);
-		registerTable.put("$s7", 0b10111);
-		registerTable.put("$t8", 0b11000);
-		registerTable.put("$t9", 0b11001);
-		registerTable.put("$k0", 0b11010);
-		registerTable.put("$k1", 0b11011);
-		registerTable.put("$gp", 0b11100);
-		registerTable.put("$sp", 0b11101);
-		registerTable.put("$fp", 0b11110);
-		registerTable.put("$ra", 0b11111);
-		
-		// This table uses the instruction name as the key and
-		// the value is the corresponding 6-bit opcode
-		// TODO: put the instructions and their 6-bit opcodes 
-		// (found on first page of book) in the hashmap
-		opcodeTable = new HashMap<String, Integer>();
-		opcodeTable.put("add", 0b000000);
-		opcodeTable.put("sub", 1);
-		opcodeTable.put("or", 1);
-		opcodeTable.put("and", 1);
-		opcodeTable.put("slt", 1);
-		opcodeTable.put("sll", 1);
-		opcodeTable.put("srl", 1);
-		opcodeTable.put("jr", 1);
-		opcodeTable.put("addi", 2);  	
-		opcodeTable.put("ori", 2);
-		opcodeTable.put("lw", 2);
-		opcodeTable.put("sw", 2);
-		opcodeTable.put("andi", 2);
-		opcodeTable.put("slti", 2);
-		opcodeTable.put("beq", 2);
-		opcodeTable.put("bne", 2);
-		opcodeTable.put("move", 2);
-		opcodeTable.put("j", 3);  		
-		opcodeTable.put("jal", 3);
-		opcodeTable.put("jr", 3);
-		
-	}
 	
 	/**
 	* Given a line of assembly code, returns the one-word
@@ -220,17 +120,21 @@ public class Assembler {
 		if(inst.equals("lw") || inst.equals("sw")) {
 			// rs is found between the parentheses
 			rsReg = line.substring(line.indexOf("(") + 1, line.indexOf(")"));
+			rtReg = line.substring(line.indexOf("$"), line.indexOf("$") + 3);
+		} else {
+			rsReg = "$t0";
+			rtReg = "$t1";
 		}
 		
 		int opcode = opcodeTable.get(inst);
-		int rs = 0b0;
-		int rt = 0b0;
-		int constant = 0b0;
+		int rs = registerTable.get(rsReg);
+		int rt = registerTable.get(rtReg);
+		int constant = 0b0000000000000000;
 		
 		int binary = opcode << 5;
 		binary = binary | rs;
 		binary = binary << 5;
-		binary = binary | rs;
+		binary = binary | rt;
 		binary = binary << 16;
 		binary = binary | constant;
 		
@@ -244,24 +148,103 @@ public class Assembler {
 	* @param line	a line of assembly code, as a String
 	* @return 		the hexadecimal representation of this instruction
 	*/
-	public static void doJ_Format(String line) {
+	public static String doJ_Format(String line) {
 		System.out.println("Made it to J-format " + line);
+		return line;
 	}
 	
 	/**
-	 * Prints a string line to a text file
-	 * 
-	 * @param hex	the line to print to the file
-	 */
-	private static void printToFile(String line) {
-		try {
-			PrintWriter pr = new PrintWriter("output.txt");	
-			pr.println(line);
-			pr.close();
-			
-		} catch(Exception e) {
-			System.out.println(e);
-		}
+	* This sets up the HashTables for the instruction types and 
+	* registers
+	*/
+	public static void setup() {
+		// TODO: We are missing a few instructions, such as bne and jr
+		
+		// Instruction type 1 codes are Register codes
+		// Instruction type 2 codes are Immediate codes
+		// lw, sw, addi, ori, andi, slti, beq, e.g. are Immediate codes
+		// add, sub, or, and, slt, sll, srl, jr e.g. are Register codes
+		// j and jal are Jump Commands
+		instTypeTable = new HashMap<String, Integer>();
+		instTypeTable.put("add", 1);  	//Register codes
+		instTypeTable.put("sub", 1);
+		instTypeTable.put("or", 1);
+		instTypeTable.put("and", 1);
+		instTypeTable.put("slt", 1);
+		instTypeTable.put("sll", 1);
+		instTypeTable.put("srl", 1);
+		instTypeTable.put("jr", 1);
+		instTypeTable.put("addi", 2);  	//Immediate codes
+		instTypeTable.put("ori", 2);
+		instTypeTable.put("lw", 2);
+		instTypeTable.put("sw", 2);
+		instTypeTable.put("andi", 2);
+		instTypeTable.put("slti", 2);
+		instTypeTable.put("beq", 2);
+		instTypeTable.put("bne", 2);
+		instTypeTable.put("j", 3);  		//Jump Commands
+		instTypeTable.put("jal", 3); 
+		
+		// The register table
+		// The registers are translated from their common name, such as
+		// t0, to their 5-bit binary representation.
+		registerTable = new HashMap<String, Integer>();
+		registerTable.put("$zero", 0b00000);
+		registerTable.put("$at", 0b00001);
+		registerTable.put("$v0", 0b00010);
+		registerTable.put("$v1", 0b00011);
+		registerTable.put("$a0", 0b00100);
+		registerTable.put("$a1", 0b00101);
+		registerTable.put("$a2", 0b00110);
+		registerTable.put("$a3", 0b00111);
+		registerTable.put("$t0", 0b01000);
+		registerTable.put("$t1", 0b01001);
+		registerTable.put("$t2", 0b01010);
+		registerTable.put("$t3", 0b01011);
+		registerTable.put("$t4", 0b01100);
+		registerTable.put("$t5", 0b01101);
+		registerTable.put("$t6", 0b01110);
+		registerTable.put("$t7", 0b01111);
+		registerTable.put("$s0", 0b10000);
+		registerTable.put("$s1", 0b10001);
+		registerTable.put("$s2", 0b10010);
+		registerTable.put("$s3", 0b10011);
+		registerTable.put("$s4", 0b10100);
+		registerTable.put("$s5", 0b10101);
+		registerTable.put("$s6", 0b10110);
+		registerTable.put("$s7", 0b10111);
+		registerTable.put("$t8", 0b11000);
+		registerTable.put("$t9", 0b11001);
+		registerTable.put("$k0", 0b11010);
+		registerTable.put("$k1", 0b11011);
+		registerTable.put("$gp", 0b11100);
+		registerTable.put("$sp", 0b11101);
+		registerTable.put("$fp", 0b11110);
+		registerTable.put("$ra", 0b11111);
+		
+		// This table uses the instruction name as the key and
+		// the value is the corresponding 6-bit opcode or function
+		// TODO: put the instructions and their 6-bit opcodes 
+		// (found on first page of book) in the hashmap
+		opcodeTable = new HashMap<String, Integer>();
+		opcodeTable.put("add", 0b100000);
+		opcodeTable.put("sub", 0b100010);
+		opcodeTable.put("or", 0b100101);
+		opcodeTable.put("and", 0b100100);
+		opcodeTable.put("slt", 0b101010);
+		opcodeTable.put("sll", 0b000000);
+		opcodeTable.put("srl", 0b000001);
+		opcodeTable.put("addi", 0b001000);  	
+		opcodeTable.put("ori", 0b001101);
+		opcodeTable.put("lw", 0b100011);
+		opcodeTable.put("sw", 0b1001011);
+		opcodeTable.put("andi", 0b001100);
+		opcodeTable.put("slti", 0b001010);
+		opcodeTable.put("beq", 0b000100);
+		opcodeTable.put("bne", 0b000101);
+		opcodeTable.put("j", 0b000010);  		
+		opcodeTable.put("jal", 0b000011);
+		opcodeTable.put("jr", 0b001000);
 		
 	}
 }
